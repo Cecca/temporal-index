56c9b0c5db580d5c74e9a686e84fcda9c6fe976d
Matteo Ceccarello <mceccarello@unibz.it>
2020-10-12 16:26:47 +0200

    Move algorithms sources to their own  directory

---
+++ src/algorithms.rs
@@ -0,0 +1,21 @@
+pub mod btree;
+pub mod ebi;
+pub mod grid;
+pub mod grid3d;
+pub mod interval_tree;
+pub mod naive;
+pub mod nested_btree;
+pub mod nested_vecs;
+pub mod period_index;
+pub mod period_index_plusplus;
+
+pub use btree::*;
+pub use ebi::*;
+pub use grid::*;
+pub use grid3d::*;
+pub use interval_tree::*;
+pub use naive::*;
+pub use nested_btree::*;
+pub use nested_vecs::*;
+pub use period_index::*;
+pub use period_index_plusplus::*;
