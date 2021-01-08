942c81a06d0f250b657b8be06d487d6559347b18
Matteo Ceccarello <mceccarello@unibz.it>
2021-01-08 17:24:47 +0100

    Start cleaning the analysis

---
+++ R/latex.R
@@ -0,0 +1,34 @@
+# This file collects functions related to the production of latex tables
+
+latex_best <- function(data_best) {
+    # TODO come up with a good naming scheme for workloads
+    data_best %>%
+        ungroup() %>%
+        select(dataset_id, queryset_id, algorithm_name, qps) %>%
+        group_by(dataset_id, queryset_id) %>%
+        mutate(
+            qps_num = qps,
+            qps = drop_units(qps) %>% scales::number(big.mark = "\\\\,"),
+            qps = if_else(qps_num == max(qps_num),
+                str_c("\\underline{", qps, "}"),
+                qps
+            )
+        ) %>%
+        mutate(
+            algorithm_name = case_when(
+                algorithm_name == "period-index++" ~ "PI++",
+                algorithm_name == "BTree" ~ "BT",
+                algorithm_name == "grid-file" ~ "GF",
+                algorithm_name == "period-index-*" ~ "PI*",
+                algorithm_name == "interval-tree" ~ "IT",
+                TRUE ~ algorithm_name
+            ),
+            algorithm_name = fct_reorder(algorithm_name, qps)
+        ) %>%
+        select(
+            dataset = dataset_id, queryset = queryset_id,
+            algorithm_name, qps
+        ) %>%
+        pivot_wider(names_from = "algorithm_name", values_from = "qps") %>%
+        kbl(format = "latex", booktabs = T, escape = F)
+}
--- _drake.R
+++ _drake.R
@@ -2,6 +2,7 @@ source("R/packages.R")
 source("R/workloads.R")
 source("R/functions.R")
 source("R/tables.R")
+source("R/latex.R")
 source("R/plan.R")
 
 drake_config(plan, lock_envir = FALSE)
