48aac4008bc22fa98df0423ef7e166ae0e545be1
Matteo Ceccarello <matteo.ceccarello@unipd.it>
2024-05-28 18:58:26 +0200

    start work on parallel indexing

--- src/algorithms/rd_index.rs
+++ src/algorithms/rd_index.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use crate::types::*;
 
 #[derive(Debug, Clone, Copy, Deserialize, Serialize)]
@@ -244,7 +246,7 @@ impl Grid {
         use std::iter::FromIterator;
 
         match order {
-            DimensionOrder::TimeDuration => Self::TimeDuration(TimePartition::new(
+            DimensionOrder::TimeDuration => Self::TimeDuration(TimePartition::par_new(
                 intervals,
                 page_size * page_size,
                 |column| {
@@ -612,6 +614,64 @@ impl<V> TimePartition<V> {
             values.push(inner_builder(&mut intervals[start..=end]));
             start = end + 1;
         }
+
+        // do the cumulative maximum end time.
+        // we need to do this because early buckets might contain end times that are later
+        // than the end times of the following buckets.
+        let mut cum_max = 0;
+        for t in max_end_times.iter_mut() {
+            if *t > cum_max {
+                cum_max = *t;
+            }
+            *t = cum_max;
+        }
+
+        Self {
+            min_start_times: max_start_times,
+            max_end_times,
+            values,
+            size: intervals.len(),
+        }
+    }
+
+    #[allow(dead_code)]
+    fn par_new<B: FnMut(&mut [Interval]) -> V>(
+        intervals: &mut [Interval],
+        block: usize,
+        mut inner_builder: B,
+    ) -> Self {
+        use rayon::prelude::*;
+
+        let mut max_start_times = Vec::new();
+        let mut max_end_times = Vec::new();
+
+        let timer = Instant::now();
+        intervals.sort_unstable_by_key(|interval| interval.start);
+        log::info!(">>> Sorting {:?}", timer.elapsed());
+
+        let mut start = 0usize;
+
+        let mut subs = Vec::new();
+
+        let timer = Instant::now();
+        while start < intervals.len() {
+            let end = next_breakpoint(intervals, start, block, |interval| interval.start);
+            max_start_times.push(intervals[start].start);
+            max_end_times.push(
+                intervals[start..=end]
+                    .iter()
+                    .map(|interval| interval.end)
+                    .max()
+                    .unwrap(),
+            );
+            subs.push(intervals[start..=end].to_owned());
+            start = end + 1;
+        }
+        log::info!(">>> Breakpoints {:?}", timer.elapsed());
+
+        let timer = Instant::now();
+        let values = subs.iter_mut().map(|sub| inner_builder(sub)).collect();
+        log::info!(">>> Columns {:?}", timer.elapsed());
 
         // do the cumulative maximum end time.
         // we need to do this because early buckets might contain end times that are later
