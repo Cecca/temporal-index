b85acb1b5ce2abc883a69ed441c933f79c4432c9
Matteo Ceccarello <matteo.ceccarello@unipd.it>
2024-05-29 13:54:59 +0200

    add run-parallel.sh

---
+++ run-parallel.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Runs the parallel experiments using different number of threads to build the index.
+
+for THREADS in 1 2 4 8 16 32; do
+	env RAYON_NUM_THREADS=$THREADS cargo run --release -- experiments/parallel.yml
+done
