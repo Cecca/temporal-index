bda349c3b106d8e0b3ae55b7429b05a3a8eb2726
Matteo Ceccarello <matteo.ceccarello@unipd.it>
2024-06-06 15:08:24 +0200

    add configuration for focus-parallel experiments

--- run-parallel.sh
+++ run-parallel.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# reset the old results
-sqlite3 temporal-index-results.sqlite "delete from parallel_raw;"
-
-# Runs the parallel experiments using different number of threads to build the index.
-
-for THREADS in 1 2 4 8 16 32; do
-	env RAYON_NUM_THREADS=$THREADS cargo run --release -- experiments/parallel.yml
-	env RAYON_NUM_THREADS=$THREADS cargo run --release -- experiments/parallel-queries.yml
+for THREADS in 1 2 4 8 16; do
+	env RAYON_NUM_THREADS=$THREADS cargo run --release -- experiments/focus-parallel.yml
 done
